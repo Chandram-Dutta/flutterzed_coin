f80bb6726fd903314a9a40055bc9c5b7362b3ada
Chandram-Dutta <chandramdutta2004@gmail.com>
2022-01-21 19:32:28 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/flutterzed_coin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterzed_coin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,144 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:neumorphic_container/neumorphic_container.dart';
+import 'package:web3dart/web3dart.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Client httpClient;
+  late Web3Client ethClient;
+  final myAddress = "0xEdBb3D1339115fFBeF919111589C651e196adcb1";
+  bool data = false;
+  int _value = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("FlutterZed's Coin"),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.grey[900],
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: 20,
+          ),
+          NeumorphicContainer(
+            spread: 2,
+            depth: 100,
+            height: 150,
+            width: MediaQuery.of(context).size.width - 20,
+            borderRadius: 10,
+            primaryColor: Colors.grey[900],
+            curvature: Curvature.flat,
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Balance",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                data
+                    ? const Padding(
+                        padding: EdgeInsets.all(25.0),
+                        child: Text("1"),
+                      )
+                    : const Padding(
+                        padding: EdgeInsets.all(25.0),
+                        child: CircularProgressIndicator(),
+                      )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          NeumorphicContainer(
+            spread: 2,
+            depth: 100,
+            width: MediaQuery.of(context).size.width - 20,
+            borderRadius: 10,
+            primaryColor: Colors.grey[900],
+            curvature: Curvature.flat,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    NeumorphicContainer(
+                      spread: 2,
+                      depth: 100,
+                      borderRadius: 10,
+                      primaryColor: Colors.grey[900],
+                      curvature: Curvature.flat,
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Text(_value.toString()),
+                      ),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.7,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Slider(
+                          min: 0.0,
+                          max: 100.0,
+                          value: _value.toDouble(),
+                          onChanged: (value) {
+                            setState(() {
+                              _value = value.toInt();
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text("Refresh"),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text("Deposit"),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text("Withdraw"),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
